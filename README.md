38a768798bfde2f4cdd5d8b2b92226f5b10f776b
panzerkat <panzerkat@tutanota.com>
2022-07-02 22:30:39 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+TinyExile is a text adventure written in C, following [Ruud Helderman's
+tutorial](https://helderman.github.io/htpataic/).
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Welcome to Exile.\n");
+    printf("\n");
+    printf("login: admin"\n);
+    printf("secret: **************");
+    printf("> return status: quiescent");
+    printf("Goodbye.\n");
+    return 0;
+}
